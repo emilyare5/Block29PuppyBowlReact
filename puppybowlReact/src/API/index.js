876e12c86960b52f55f434f1877356722fc18693
emilyare5 <emilyarellano1819@gmail>
2024-02-22 15:34:40 -0500

    style: add single puppy info

--- puppybowlReact/src/API/index.js
+++ puppybowlReact/src/API/index.js
@@ -1,4 +1,4 @@
-
+import { useParams } from 'react-router-dom'
 
 export async function getAllPlayers() {
     try {
@@ -15,6 +15,33 @@ export async function getAllPlayers() {
         console.error(error);
     }
 }
+
+export async function getSinglePlayer(bookID){
+
+        // console.log(bookID)
+    
+    try {
+        const response = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2311-FTB-ET-WEB-PT/players/${bookID}`)
+        // console.log(response)
+        const result = await response.json()
+        // console.log(result)
+        const resultplayers = result.data.player
+        // console.log(resultplayers)
+        return resultplayers
+        
+
+
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
+
+
+
+
+
 
 
 
--- puppybowlReact/src/components/SinglePlayer.jsx
+++ puppybowlReact/src/components/SinglePlayer.jsx
@@ -1,13 +1,45 @@
 import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import { getSinglePlayer } from '../API';
 
 
-export default function Singleplayer(){
 
-    let {bookID} = useParams()
 
-    return(
+export default function Singleplayer() {
+
+    const [singleData, setSingleData] = useState(null)
+
+    let { bookID } = useParams()
+
+    useEffect(() => {
+
+        async function single() {
+            const data = await getSinglePlayer(bookID)
+            setSingleData(data)
+
+        }
+
+        single()
+    }, [bookID])
+
+    console.log(singleData)
+
+    return (
         <div>
-            <h2>Single Player id {bookID}</h2>
+            {singleData ? (
+                <div>
+                    <h2>{singleData.name}</h2>
+                    <p>Id: {singleData.id}</p>
+                    <img src={singleData.imageUrl}/>
+                    <p>Breed: {singleData.breed}</p>
+                    <p>Status: {singleData.status}</p>
+                    <p>Cohort Id: {singleData.cohortId}</p>
+                    <p> Created at: {singleData.createdAt}</p>
+                    <p> Updated at: {singleData.updatedAt}</p>
+                </div>
+            ) : (
+                <p>Loading...</p>
+            )}
         </div>
     )
 }
