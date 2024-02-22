99faf269fdc6a15e117a6862eda53863febee0cc
emilyare5 <emilyarellano1819@gmail>
2024-02-22 15:35:00 -0500

    style: add image css

--- puppybowlReact/src/main.jsx
+++ puppybowlReact/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import './con.css'
 // we need this here so we can access it everywhere
 
 ReactDOM.createRoot(document.getElementById('root')).render(
